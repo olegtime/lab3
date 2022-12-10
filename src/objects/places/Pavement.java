68266dcb3992f03647faf86c754f6cb14a23c5ea
olegtime <blueberry.reveror@gmail.com>
2022-12-10 19:50:10 +0300

    v.1.1

--- src/objects/places/Pavement.java
+++
@@ -1,7 +0,0 @@
-package objects.places;
-
-public class Pavement extends Place{
-    public Pavement(String name){
-        super(name);
-    }
-}
--- src/objects/places/PoliceDepartment.java
+++
@@ -1,7 +0,0 @@
-package objects.places;
-
-public class PoliceDepartment extends Place{
-    public PoliceDepartment(String name){
-        super(name);
-    }
-}
